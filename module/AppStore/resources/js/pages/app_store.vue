7cea68f93d3c29960dbb2b37ef44bc38b037bd54
liusq <921491025@qq.com>
2023-05-15 16:35:16 +0800

    应用市场更新

--- module/AppStore/resources/js/pages/app_store.vue
+++ module/AppStore/resources/js/pages/app_store.vue
@@ -1,6 +1,15 @@
 <template>
-    <div class="content_container">
-        AppStore
+    <div class="content_container" style="margin: 5px">
+        <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane name="appstore" label="模块市场">
+            </el-tab-pane>
+            <el-tab-pane name="installed" label="已安装">
+            </el-tab-pane>
+            <el-tab-pane name="enabled" label="已启用">
+            </el-tab-pane>
+            <el-tab-pane name="disabled" label="已禁用">
+            </el-tab-pane>
+        </el-tabs>
     </div>
 </template>
 
@@ -10,14 +19,16 @@
 export default {
     data() {
         return {
-
+            activeName: 'appstore'
         }
     },
     created() {
 
     },
     methods: {
-
+        handleClick(tab, event) {
+            console.log(tab, event);
+        }
 
     }
 }
