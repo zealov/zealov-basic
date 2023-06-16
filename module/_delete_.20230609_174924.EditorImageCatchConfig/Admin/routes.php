d5fa3ec8dae5f8852832ab830a1f0e9a64b22aa8
liusq <921491025@qq.com>
2023-06-16 16:51:21 +0800

    update

--- module/_delete_.20230609_174924.EditorImageCatchConfig/Admin/routes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-/* @var \Illuminate\Routing\Router $router */
-
-$router->match(['get', 'post'], 'editor_image_catch_config/config', 'ConfigController@index');
--- module/_delete_.20230609_174924.EditorImageCatchConfig/Docs/module/preview.md
+++
@@ -1,1 +0,0 @@
-![image-20220712152128529](https://ms-assets.modstart.com/data/image/2022/07/12/26490_y6ko_8024.png)
